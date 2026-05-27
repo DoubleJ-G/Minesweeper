b240caf6952e473fbcd2f9112bc1bbd3532e95de
Jaden Gregory <jadengregory@MacBook-Pro.local>
2026-05-27 11:28:40 +1000

    Ignore coverage folder for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,10 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
+const ignores = ["dist", "coverage"];
+
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(ignores),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [

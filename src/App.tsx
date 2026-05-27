1a26c5a5bea7576e3ed75e9b3ad3bc374803577e
Jaden Gregory <jadengregory@MacBook-Pro.local>
2026-05-27 11:01:37 +1000

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <main>Hello World</main>;
+  return (
+    <main className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-5xl font-bold">Hello World</h1>
+    </main>
+  );
 }
 
 export default App;

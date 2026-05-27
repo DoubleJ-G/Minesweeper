2e6e1ebf3d01789eb3aef67eeb8b30ad7c3493fa
Jaden Gregory <jadengregory@MacBook-Pro.local>
2026-05-27 10:55:08 +1000

    Add tests

---
+++ src/App.spec.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+import App from "./App";
+import { screen, render } from "@testing-library/react";
+
+describe("App", () => {
+  it("renders the App component", () => {
+    render(<App />);
+
+    const el = screen.getByText("Hello World");
+
+    expect(el).toBeDefined();
+  });
+});
